8a83736ccf6945b3478aa4decbfbe4b3083f339c
Seung Moon <moonsw89@gmail.com>
2020-03-09 22:17:17 -0400

    Module 6 complete

--- mod6-solution/js/app.js
+++ mod6-solution/js/app.js
@@ -6,6 +6,26 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    
+    $scope.lunch = "";
+    $scope.answer = "";
+    $scope.class = "";
+
+    $scope.checkLunch = function () {
+      if (!$scope.lunch || $scope.lunch === "") {
+        $scope.class = "red";
+        $scope.answer = "Please enter data first"
+      } else {
+        $scope.class = "green";
+        var lunchList = $scope.lunch.split(",");
+        var filteredLunchList = lunchList.filter(function (item) {
+          return item != null && item.trim() !== "";
+        });
+        if (filteredLunchList.length <= 3) {
+          $scope.answer = "Enjoy!";
+        } else {
+          $scope.answer = "Too much!";
+        }
+      }
+    };
   }
 })();
